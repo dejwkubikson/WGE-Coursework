0f063363d35bdc8f0fe85bf6dbb0a23939752571
dejwkubikson <44845403+dejwkubikson@users.noreply.github.com>
2019-03-12 20:18:54 +0000

    Started Scene 2

---
+++ Assets/Scripts/Scene 2/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraSpeed = 0.2f;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // In LateUpdate() the player object was a bit 'woobly' when moving
+    void FixedUpdate()
+    {
+        Vector3 playerPos = player.transform.position + offset;
+        Vector3 lerpedPos = Vector3.Lerp(transform.position, playerPos, cameraSpeed * cameraSpeed);
+        transform.position = lerpedPos;
+    }
+}
---
+++ Assets/Scripts/Scene 2/PlayerScript2D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with " + collision.gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
