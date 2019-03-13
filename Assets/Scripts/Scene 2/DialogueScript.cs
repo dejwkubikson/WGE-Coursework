35158cd2c29086f6a942b33263e329d4a189a357
dejwkubikson <44845403+dejwkubikson@users.noreply.github.com>
2019-03-13 21:45:44 +0000

    Working on Scene 2 dialogues

---
+++ Assets/Scripts/Scene 2/DialogueScript.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public class DialogueScript : MonoBehaviour
+{
+    public string dialogueFileName = "DialogueData.xml";
+    public bool dialogueStarted = false;
+    public bool dialogueEnded = false;
+
+
+    private string speaker = "";
+
+    public void StartDialogue()
+    {
+        dialogueStarted = true;
+
+        LoadDialogueFromXMLFile(dialogueFileName);
+    }
+
+    public void LoadDialogueFromXMLFile(string fileName)
+    {
+        // Checking if the file has XML ending
+        if (!(fileName.Contains(".xml")))
+            fileName += ".xml";
+
+        // Checking if the file exists
+        if (!(System.IO.File.Exists(fileName)))
+        {
+            Debug.Log("File " + fileName + " wasn't found!");
+            return;
+        }
+
+        // Create an XML reader with the file supllied
+        XmlReader xmlReader = XmlReader.Create(fileName);
+
+        while(xmlReader.Read())
+        {
+            // Getting the name of the speaker
+            if(xmlReader.IsStartElement("speaker"))
+            {
+                speaker = xmlReader.Value;
+                Debug.Log("Speaker name: " + speaker);
+            }
+
+        }
+
+    }
+    /*
+    <speaker>NPC</speaker>
+	
+	<conversation id = "start" >
+
+        < text id="start">Hey, how's it going?</text>
+		<options>
+			<option next = "option1" > I'm doing okay.</option>
+			<option next = "option2" > I'm doing terribly.</option>
+			<option next = "option3" > I'm doing Great!</option>
+		</options>
+	</conversation>
+		
+	<conversation id = "option1" >
+
+        < text > That's good to hear.</text>
+		<options>
+			<option next = "end" > Bye.</ option >
+
+        </ options >
+
+    </ conversation >
+    */
+    // Read a voxel chunk from XML file
+    public static int[,,] LoadChunkFromXMLFile(int size, string fileName)
+    {
+        int[,,] voxelArray = new int[size, size, size];
+
+        // Create ab XML reader with the file supplied
+        XmlReader xmlReader = XmlReader.Create(fileName);
+
+        // Iterate through and read every line in the XML file
+        while (xmlReader.Read() && System.IO.File.Exists(fileName))
+        {
+            if (xmlReader.IsStartElement("Voxel"))
+            {
+                // Retrieve x, y and z attributes and store as int
+                int x = int.Parse(xmlReader["x"]);
+                int y = int.Parse(xmlReader["y"]);
+                int z = int.Parse(xmlReader["z"]);
+
+                xmlReader.Read(); // Goes to another node!
+
+                int value = int.Parse(xmlReader.Value);
+
+                voxelArray[x, y, z] = value;
+            }
+        }
+        return voxelArray;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Scene 2/PlayerScript2D.cs
+++ Assets/Scripts/Scene 2/PlayerScript2D.cs
@@ -4,20 +4,55 @@ using UnityEngine;
 
 public class PlayerScript2D : MonoBehaviour
 {
+    // STARTING POS X = -12, Y = 0, Z = -7.419922
+    Vector3 playerPos;
+    Vector3 npcPos;
+    float distanceToNPC;
+    PlayerMovement2D playerMovement;
+    DialogueScript dialogueScript;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        // Getting the movement script
+        playerMovement = this.gameObject.GetComponent<PlayerMovement2D>();
+        playerPos = new Vector3();
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Collided with " + collision.gameObject.name);
+        // Getting the dialogue script
+        GameObject dialogueObject = GameObject.Find("DialogueObject").gameObject;
+        if (dialogueObject != null)
+            dialogueScript = dialogueObject.GetComponent<DialogueScript>();
+
+        // Finding the NPC object to get it's position
+        GameObject npcObject = GameObject.Find("NPC").gameObject;
+        if (npcObject != null)
+            npcPos = npcObject.transform.position;
+        else
+            Debug.Log("Couldn't find NPC!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerPos = this.transform.position;
+        distanceToNPC = Vector3.Distance(playerPos, npcPos);
+
+        // If the player is close enough to the NPC and isn't in the air
+        if(distanceToNPC < 3 && playerMovement._mState != MovementState.IN_AIR)
+        {
+            //Debug.Log("Close to the NPC.");
+
+            // We want to start the dialogue once
+            if (!(dialogueScript.dialogueStarted))
+                dialogueScript.StartDialogue();
+
+            // Stop movement if the dialogue hasn't ended
+            if (!(dialogueScript.dialogueEnded))
+                playerMovement._mState = MovementState.DISABLED;
+            else playerMovement._mState = MovementState.ON_GROUND;
+
+
+        }
+
     }
 }
